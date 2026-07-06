6f9cce3772107bdc95d49e7f9f7c6b12a8e8fd87
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "network_watchers" {
   description = "All network_watcher resources"
   value       = azurerm_network_watcher.network_watchers
 }
+output "network_watchers_location" {
+  description = "List of location values across all network_watchers"
+  value       = [for k, v in azurerm_network_watcher.network_watchers : v.location]
+}
+output "network_watchers_name" {
+  description = "List of name values across all network_watchers"
+  value       = [for k, v in azurerm_network_watcher.network_watchers : v.name]
+}
+output "network_watchers_resource_group_name" {
+  description = "List of resource_group_name values across all network_watchers"
+  value       = [for k, v in azurerm_network_watcher.network_watchers : v.resource_group_name]
+}
+output "network_watchers_tags" {
+  description = "List of tags values across all network_watchers"
+  value       = [for k, v in azurerm_network_watcher.network_watchers : v.tags]
+}
 
