9eb20fe01aa810ce8978d3e041a6ff6e235ee6c3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "network_watchers" {
-  description = "All network_watcher resources"
-  value       = azurerm_network_watcher.network_watchers
-}
 output "network_watchers_location" {
-  description = "List of location values across all network_watchers"
-  value       = [for k, v in azurerm_network_watcher.network_watchers : v.location]
+  description = "Map of location values across all network_watchers, keyed the same as var.network_watchers"
+  value       = { for k, v in azurerm_network_watcher.network_watchers : k => v.location }
 }
 output "network_watchers_name" {
-  description = "List of name values across all network_watchers"
-  value       = [for k, v in azurerm_network_watcher.network_watchers : v.name]
+  description = "Map of name values across all network_watchers, keyed the same as var.network_watchers"
+  value       = { for k, v in azurerm_network_watcher.network_watchers : k => v.name }
 }
 output "network_watchers_resource_group_name" {
-  description = "List of resource_group_name values across all network_watchers"
-  value       = [for k, v in azurerm_network_watcher.network_watchers : v.resource_group_name]
+  description = "Map of resource_group_name values across all network_watchers, keyed the same as var.network_watchers"
+  value       = { for k, v in azurerm_network_watcher.network_watchers : k => v.resource_group_name }
 }
 output "network_watchers_tags" {
-  description = "List of tags values across all network_watchers"
-  value       = [for k, v in azurerm_network_watcher.network_watchers : v.tags]
+  description = "Map of tags values across all network_watchers, keyed the same as var.network_watchers"
+  value       = { for k, v in azurerm_network_watcher.network_watchers : k => v.tags }
 }
 
