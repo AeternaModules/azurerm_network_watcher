48bd88bb20a5e338066e3e99320ab2824f6fe125
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "network_watchers_id" {
+  description = "Map of id values across all network_watchers, keyed the same as var.network_watchers"
+  value       = { for k, v in azurerm_network_watcher.network_watchers : k => v.id }
+}
 output "network_watchers_location" {
   description = "Map of location values across all network_watchers, keyed the same as var.network_watchers"
   value       = { for k, v in azurerm_network_watcher.network_watchers : k => v.location }
